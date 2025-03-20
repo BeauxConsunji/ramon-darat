53a748e2b84970381607fc151e3977bf79e27d3b
Jaim <131182310+jaim-kia@users.noreply.github.com>
2025-03-20 17:55:17 +0800

    wash rice minigame functions complete

--- Assets/GameSystem/Cooking/Draggable.cs
+++ Assets/GameSystem/Cooking/Draggable.cs
@@ -6,7 +6,6 @@ using UnityEngine;
 public class Draggable : MonoBehaviour
 {
     public enum Type { None, Knife, Stirrer, Skin, Ingredient }
-
     public Type type = Type.None;
     private Plane dragPlane;
     private Vector3 offset;
@@ -14,14 +13,12 @@ public class Draggable : MonoBehaviour
     void OnMouseDown() {
         dragPlane = new Plane(G.I.mainCamera.transform.forward, transform.position);
         Ray camRay = G.I.mainCamera.ScreenPointToRay(Input.mousePosition);
-
         float planeDist;
         dragPlane.Raycast(camRay, out planeDist);
         offset = transform.position - camRay.GetPoint(planeDist);
     }
     void OnMouseDrag() {
         Ray camRay = G.I.mainCamera.ScreenPointToRay(Input.mousePosition);
-
         float planeDist;
         dragPlane.Raycast(camRay, out planeDist);
         transform.position = camRay.GetPoint(planeDist) + offset;
