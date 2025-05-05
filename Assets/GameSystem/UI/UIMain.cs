3f0ed88eeed6169650c396623f873e8716323f28
Jaim <131182310+jaim-kia@users.noreply.github.com>
2025-05-05 23:21:18 +0800

    bg of the music

--- Assets/GameSystem/UI/UIMain.cs
+++ Assets/GameSystem/UI/UIMain.cs
@@ -148,13 +148,13 @@ public class UIMain : UIView<UIMainState> {
                                             description = "The Philippines’ unofficial national dish. Rich, savory, and deeply comforting — a Filipino classic that tastes like home.",
                                             thumbnail = Resources.Load<Sprite>("Thumbnails/recipe-selection-adobo"),
                                             finalImage = Resources.Load<Sprite>("Sprite/Food/adobo_final"),
-                                            gameObjectId="AdoboRecipe",
-                                            locked=false
+                                            gameObjectId="AdoboRecipe"
                                         },
                     new UIRecipeState() { name = "Pinakbet",
                                             description = "A traditional Filipino vegetable stew— healthy, hearty, and bursting with flavor.",
+                                            thumbnail = Resources.Load<Sprite>("Thumbnails/recipe-selection-pinakbet"),
                                             finalImage = Resources.Load<Sprite>("Thumbnails/recipe-selection-pinakbet"),
-                                            thumbnail = Resources.Load<Sprite>("Sprite/Food/adobo_final")
+
                                         },
                     new UIRecipeState() { name = "Sinigang",
                                             description = "Maasim",
